0f562c8634c36d87fab4d8110fbe26a58cc3f48a
okrzyzos <okrzyzos@outlook.fr>
2023-11-29 17:17:28 +0100

    feat: add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
-import LoginPage from "./login/LoginPage";
+import LoginPage from "./components/pages/login/LoginPage";
+import OrderPage from "./components/pages/login/OrderPage";
+import PageErreur from "./components/pages/login/PageErreur";
 
 function App() {
-
   return (
     <>
-      <LoginPage />
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<LoginPage />} />
+          <Route path="/order-page" exact element={<OrderPage />} />
+          <Route path="*" element={<PageErreur />} />{" "}
+        </Routes>
+      </Router>
     </>
   );
 }
