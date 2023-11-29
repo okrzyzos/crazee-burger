fec20cdbdc6917fe2aa5aa416c306225c2771b20
okrzyzos <okrzyzos@outlook.fr>
2023-11-29 13:38:39 +0100

    refactor: add component Loginform

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+import './LoginPage.css';
+
+export const LoginForm = () => {
+
+    const [firstName, setFirstName] = useState('');
+
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      if (firstName) {
+        alert(`Bonjour ${firstName}`);
+        setFirstName('');
+      } 
+    };
+  return (
+    <div className='container-form'>
+    <h1>Bienvenue chez nous !</h1>
+    <h3>Connectez-vous </h3>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={firstName}
+        onChange={(e) => setFirstName(e.target.value)}
+        placeholder="Entrez votre prénom..."
+        required
+      />
+   
+      <button type="submit">Accéder à votre espace</button>
+    </form>
+  </div>
+  )
+}
