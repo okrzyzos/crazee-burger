ff720bd2ffc6d27cf5f7858d41ed9a56f2dadbdd
okrzyzos <okrzyzos@outlook.fr>
2023-11-29 17:56:51 +0100

    refactor: update routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./components/pages/login/LoginPage";
 import OrderPage from "./components/pages/login/OrderPage";
@@ -7,13 +7,11 @@ import PageErreur from "./components/pages/login/PageErreur";
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
+      <Routes>
           <Route path="/" exact element={<LoginPage />} />
           <Route path="/order-page" exact element={<OrderPage />} />
           <Route path="*" element={<PageErreur />} />{" "}
         </Routes>
-      </Router>
     </>
   );
 }
