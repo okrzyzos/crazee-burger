e8c427761cebfb6d72c8789482b14c55bda1baa6
okrzyzos <okrzyzos@outlook.fr>
2023-12-02 23:36:33 +0100

    fix: add className props

---
+++ src/components/pages/Navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import styled from "styled-components";
+import NavbarRightSide from "./NavbarRightSide";
+import Logo from "../reusable-ui/Logo";
+import { refresh } from "../../../utils/window";
+import theme from "../../../theme/index";
+
+
+function Navbar({ username }) {
+  return (
+    <NavbarStyled>
+      <Logo  fontSize="55px" className="logo" onClick={refresh} />
+
+      <NavbarRightSide username={username} />
+    </NavbarStyled>
+  );
+}
+
+const NavbarStyled = styled.nav`
+  background-color: #ffffff;
+  height: 10vh;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 30px;
+  cursor: pointer;
+  font-size: 20px;
+  border-radius: 15px 15px 1px 1px;
+  border-bottom: 1px solid ${theme.colors.greyLight};
+
+  .logo {
+   width: 100px;
+  }
+`;
+
+export default Navbar;
