7f7c7debbc0ee6bab3426a66198e7e8a031bfe89
okrzyzos <okrzyzos@outlook.fr>
2023-12-03 16:50:11 +0100

    feat: add import toastcontainer

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,17 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import {  toast, ToastContainer } from "react-toastify";
+
+
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
       <App />
+      <ToastContainer  position="bottom-right" />
     </BrowserRouter>
   </React.StrictMode>
 );
