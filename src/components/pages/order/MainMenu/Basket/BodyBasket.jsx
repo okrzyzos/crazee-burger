47511ddd1cfaae665b4fb914aaee9de9dcec9423
okrzyzos <okrzyzos@outlook.fr>
2023-12-09 22:17:38 +0100

    refactor: create  body head footer basket

---
+++ src/components/pages/order/MainMenu/Basket/BodyBasket.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function BodyBasket() {
+  return (
+    <div className="body">Votre commande est vide</div>
+  )
+}
+
+export default BodyBasket
---
+++ src/components/pages/order/MainMenu/Basket/FooterBasket.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function FooterBasket() {
+  return (
+    <div className="footer">Codé avec ❤️ et React.JS</div>
+  )
+}
+
+export default FooterBasket
