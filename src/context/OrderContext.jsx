ae350a4680d038b394565662d821bb20e9b059df
okrzyzos <okrzyzos@outlook.fr>
2023-12-09 15:50:20 +0100

    feat: add function in context

--- src/context/OrderContext.jsx
+++ src/context/OrderContext.jsx
@@ -8,6 +8,19 @@ export default createContext({
   currentTabSelected: false,
   setCurrentTabSelected: () => {},
   addProductToMenu: () => {},
+  updateProductInMenu: () => {},
   menuData: [],
   setMenuData: () => {},
+  handleAdd: () => {},
+  handleDelete: () => {},
+  handleEdit: () => {},
+  resetMenu: () => {},
+
+  newProduct: {},
+  setNewProduct: () => {},
+
+  productSelected: {},
+  setProductSelected: () => {},
+
+  titleEditRef: {},
 });
