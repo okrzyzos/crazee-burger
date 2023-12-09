ac9174f910b679aa84e6631f38b72f495db543dc
okrzyzos <okrzyzos@outlook.fr>
2023-12-09 15:54:02 +0100

    refactor: refacto file

---
+++ src/components/pages/order/MainMenu/MainRightSide/admin/tabsConfig.jsx
@@ -0,0 +1,22 @@
+import { AiOutlinePlus } from "react-icons/ai";
+import { MdModeEditOutline } from "react-icons/md";
+import AddProductForm from "./admin-panel/AddProductForm";
+import EditForm from "./admin-panel/EditForm";
+
+export const tabsConfig = [
+  {
+    index: "add",
+    label: "Ajouter un produit",
+    Icon: <AiOutlinePlus />,
+    Content: <AddProductForm />,
+  },
+  {
+    index: "edit",
+    label: "Modifier un produit",
+    Icon: <MdModeEditOutline />,
+    Content: <EditForm />,
+  },
+];
+
+export const getTabSelected = (tabs, currentTabSelected) =>
+  tabs.find((tab) => tab.index === currentTabSelected);
