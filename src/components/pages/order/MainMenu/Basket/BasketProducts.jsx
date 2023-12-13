2137733827633f6e1193421f6ea52e4df5daff35
okrzyzos <okrzyzos@outlook.fr>
2023-12-14 00:34:58 +0100

    feat: create file basketproduct

---
+++ src/components/pages/order/MainMenu/Basket/BasketProducts.jsx
@@ -0,0 +1,56 @@
+import React, { useContext } from "react";
+import BasketCard from "./BasketCard";
+import { IMAGE_COMING_SOON } from "../../../../../enums/product";
+import { RiDeleteBin2Fill } from "react-icons/ri";
+import styled from "styled-components";
+import { checkIfProductIsClicked } from "../MainRightSide/Menu/helper";
+import OrderContext from "../../../../../context/OrderContext";
+import { findObjectById } from "../../../../../utils/array";
+
+export default function BasketProducts() {
+  const { menuData, isAdminMode, handleDeleteBasket, basket,handleProductSelected,productSelected } =
+    useContext(OrderContext);
+
+  
+  const handleOnDelete = (event, id) => {
+    event.stopPropagation();
+    handleDeleteBasket(id);
+  };
+
+  return (
+    <div>
+      {basket.map((menu) => {
+        const menuProduct = findObjectById(menu.id, menuData);
+        return (
+          <BasketCard
+            key={menu.id}
+            imageSource={
+              menuProduct.imageSource
+                ? menuProduct.imageSource
+                : IMAGE_COMING_SOON
+            }
+            {...menuProduct}
+            isClickable={isAdminMode}
+            quantity={menu.quantity}
+            onClick={isAdminMode ? () => handleProductSelected(menu.id) : null}
+            isSelected={checkIfProductIsClicked(menu.id, productSelected.id)}
+          >
+            <DeleteIcon onClick={(event) => handleOnDelete(event, menu.id)} />
+          </BasketCard>
+        );
+      })}
+    </div>
+  );
+}
+
+const DeleteIcon = styled(RiDeleteBin2Fill)`
+  color: red;
+  cursor: pointer;
+  font-size: 20px;
+  transition: color 0.3s;
+
+  &:hover {
+    color: red;
+    cursor: pointer;
+  }
+`;
