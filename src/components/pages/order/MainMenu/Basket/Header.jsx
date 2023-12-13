a36560e7cd377e44f60b91d7cce2f7b2f7eea7c1
okrzyzos <okrzyzos@outlook.fr>
2023-12-14 00:35:54 +0100

    feat: create file header

--- src/components/pages/order/MainMenu/Basket/Header.jsx
+++ src/components/pages/order/MainMenu/Basket/Header.jsx
@@ -1,23 +1,33 @@
-import React from "react";
-import styled from 'styled-components';
-
+import React, { useContext } from "react";
+import styled from "styled-components";
+import OrderContext from "../../../../../context/OrderContext";
+import { formatPrice } from "../../../../../utils/maths";
+import { findObjectById } from "../../../../../utils/array";
 
 function Header() {
+  const { basket,menuData } = useContext(OrderContext);
+
+   const calculateSumToPay = () => {
+    return basket.reduce((total, basketProduct) => {
+      const menuProduct = findObjectById(basketProduct.id, menuData)
+      if (isNaN(menuProduct.price)) return total
+      total += menuProduct.price * basketProduct.quantity
+      return total
+    }, 0)
+  }
   return (
     <HeaderStyled>
       <span className="text-total">Total </span>
-      <span className="text-total">0,00€ </span>
+      <span className="text-total">{formatPrice(calculateSumToPay())} </span>
     </HeaderStyled>
   );
 }
 
-
 const HeaderStyled = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 15px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
 `;
-
 
 export default Header;
