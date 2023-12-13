47ddc6cb0d2fd95ab4bc5f04fd39b21914d767a3
okrzyzos <okrzyzos@outlook.fr>
2023-12-14 00:35:35 +0100

    feat: create footerbasket

--- src/components/pages/order/MainMenu/Basket/FooterBasket.jsx
+++ src/components/pages/order/MainMenu/Basket/FooterBasket.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
+import theme from '../../../../../theme';
+
 
 function FooterBasket() {
   return (
-    <div className="footer">Codé avec ❤️ et React.JS</div>
+    <FooterBasketStyled>Codé avec ❤️ et React.JS</FooterBasketStyled>
   )
 }
+
+
+const FooterBasketStyled = styled.div`
+ height:70px;
+ display:flex;
+ justify-content:center;
+ align-items:center;
+`;
+
 
 export default FooterBasket
