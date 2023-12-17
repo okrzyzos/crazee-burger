727a644522eabb2e3cf8dc7b3ff6614f7cfc927d
okrzyzos <okrzyzos@outlook.fr>
2023-12-17 19:57:01 +0100

    refactor: refacto file

--- src/enums/product.js
+++ src/enums/product.js
@@ -9,3 +9,12 @@ export const IMAGE_COMING_SOON = "/images/coming-soon.png";
 
 
 export default EMPTY_PR0DUCT;
+
+export const BASKET_MESSAGE = {
+  EMPTY:"Votre commaande est vide",
+  LOADING:"Chargement en cours..."
+}
+
+
+
+
