06a0733a51fdcab22e4bd2d00e1544e77b03be8a
okrzyzos <okrzyzos@outlook.fr>
2023-12-22 22:44:05 +0100

    fix: update function calculate

--- src/components/pages/order/MainMenu/Basket/Header.jsx
+++ src/components/pages/order/MainMenu/Basket/Header.jsx
@@ -3,23 +3,26 @@ import styled from "styled-components";
 import OrderContext from "../../../../../context/OrderContext";
 import { formatPrice } from "../../../../../utils/maths";
 import { findObjectById } from "../../../../../utils/array";
+import CasinoEffect from "../../../reusable-ui/CasinoEffect";
+import { isAvailableOptions } from '../../../../../enums/select';
+import { convertStringToBoolean } from '../../../../../utils/string';
 
 function Header() {
-  const { basket,menuData } = useContext(OrderContext);
+  const { basket, menuData } = useContext(OrderContext);
 
-
-   const calculateSumToPay = () => {
+  const calculateSumToPay = () => {
     return basket.reduce((total, basketProduct) => {
-      const menuProduct = findObjectById(basketProduct.id, menuData)
-      if (isNaN(menuProduct.price)) return total
-      total += menuProduct.price * basketProduct.quantity
-      return total
-    }, 0)
-  }
+      const menuProduct = findObjectById(basketProduct.id, menuData);
+      if (isNaN(menuProduct.price)) return total;
+      if(convertStringToBoolean(menuProduct.isAvailable) === false) return total
+      total += menuProduct.price * basketProduct.quantity;
+      return total;
+    }, 0);
+  };
   return (
     <HeaderStyled>
       <span className="text-total">Total </span>
-      <span className="text-total">{formatPrice(calculateSumToPay())} </span>
+      <CasinoEffect  count={formatPrice(calculateSumToPay())} />
     </HeaderStyled>
   );
 }
