0ed655a420bab908031e761dda2361082a1c7fd4
okrzyzos <okrzyzos@outlook.fr>
2023-12-22 22:46:39 +0100

    feat: add select

--- src/components/pages/order/MainMenu/MainRightSide/admin/admin-panel/inputTextConfig.jsx
+++ src/components/pages/order/MainMenu/MainRightSide/admin/admin-panel/inputTextConfig.jsx
@@ -1,6 +1,9 @@
 import { FaHamburger } from "react-icons/fa"
 import { BsFillCameraFill } from "react-icons/bs"
 import { MdOutlineEuro } from "react-icons/md"
+import { GoPackage } from "react-icons/go";
+import { GiMegaphone } from "react-icons/gi";
+import { isAvailableOptions, isPublicisedOptions } from "../../../../../../../enums/select"
 
 export const getInputTextsConfig = (newProduct) => [
   {
@@ -8,6 +11,7 @@ export const getInputTextsConfig = (newProduct) => [
     name: "title",
     value: newProduct.title,
     type: "text",
+    className:"title",
     placeholder: "Nom du produit (ex: Super Burger)",
     Icon: <FaHamburger />,
     version: "minimalist",
@@ -17,6 +21,7 @@ export const getInputTextsConfig = (newProduct) => [
     name: "imageSource",
     value: newProduct.imageSource,
     type: "text",
+    className:"image-source",
     placeholder: "Lien URL d'une image (ex: https://la-photo-de-mon-produit.png)",
     Icon: <BsFillCameraFill />,
     version: "minimalist",
@@ -27,7 +32,35 @@ export const getInputTextsConfig = (newProduct) => [
     value: newProduct.price ? newProduct.price : "",
     type: "text",
     placeholder: "Prix",
+    className:"price",
     Icon: <MdOutlineEuro />,
     version: "minimalist",
   },
 ]
+
+export const getSelectInputConfig = (newProduct) => [
+
+{
+  id: "3",
+  name: "isAvailable",
+  value: newProduct.isAvailable,
+  options:isAvailableOptions,
+  Icon: <GoPackage />,
+  className:"is-available",
+
+},
+{
+  id: "4",
+  name: "isPublicised",
+  value: newProduct.isPublicised,
+  options:isPublicisedOptions,
+  Icon: <GiMegaphone />,
+  className:"is-publicised",
+
+}
+
+]
+
+
+
+
